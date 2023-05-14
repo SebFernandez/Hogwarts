ca74db0b92365e5051af37a829b80fafde135f37
FlorSalva <flsalvatierra@alumno.unlam.edu.ar>
2023-05-14 13:08:29 -0300

    Borre los comentarios
    Borre el to-do

--- src/unlam/paradigmas/tp/tierramedia/Main.java
+++ src/unlam/paradigmas/tp/tierramedia/Main.java
@@ -4,15 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenido a la Tierra Media");
-		
-		/*	TODO:
-		 * 1. Crear consumo del archivo con información. Preferentemente que el archivo de input tenga la información en formato JSON.
-		 * 		- Para el uso de archivos crear las clases de input y output vistas en clase.
-		 * 2. Una vez consumido el archivo, crear diferentes usuarios, atraccions y paquetes de viaje.
-		 * 3. Crear clases de promociones, usuario, atracción, resumen de compra.
-		 * 4. Crear el ofertador de atracciones o pack según intereses del usuario.
-		 */
-		
 		System.out.println("Tierra Media le desea un lindo paseo!");
 	}
 }
