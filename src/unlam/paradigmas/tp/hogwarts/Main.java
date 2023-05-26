b33ec8bd1e5414bc070aaa837e889423e25bd2fc
Sebastián Fernandez <seba.fernandez95@gmail.com>
2023-05-26 20:27:22 -0300

    fix: corrección con los archivos nuevos.

--- src/unlam/paradigmas/tp/hogwarts/Main.java
+++ src/unlam/paradigmas/tp/hogwarts/Main.java
@@ -10,9 +10,9 @@ import static unlam.paradigmas.tp.hogwarts.servicio.Archivo.lecturaDeAtracciones
 import static unlam.paradigmas.tp.hogwarts.servicio.Archivo.lecturaDeUsuarios;
 
 public class Main {
-    private static final String RUTA_ARCHIVO_USUARIOS = "archivos/usuarios.csv";
-    private static final String RUTA_ARCHIVO_ATRACCIONES = "archivos/atracciones.csv";
-    private static final String RUTA_ARCHIVO_PROMOIONES = "archivos/promociones.csv";
+    private static final String RUTA_ARCHIVO_USUARIOS = "Archivos/preferencias_usuarios.csv";
+    private static final String RUTA_ARCHIVO_ATRACCIONES = "Archivos/Atracciones.csv";
+    private static final String RUTA_ARCHIVO_PROMOIONES = "Archivos/paquetes_a_ofrecer.csv";
 
     public static void main(String[] args) {
         System.out.println("\n\n----------------------------\t\tBienvenido a la Tierra Media\t\t----------------------------");
