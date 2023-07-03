854fac53f5e65741f7a7838ed21e4a64ddb137e7
Sebastián Fernandez <seba.fernandez95@gmail.com>
2023-07-03 19:54:23 -0300

    fix: mejora del comparator.

--- src/unlam/paradigmas/tp/hogwarts/servicio/ProductoComparator.java
+++ src/unlam/paradigmas/tp/hogwarts/servicio/ProductoComparator.java
@@ -1,48 +1,27 @@
 package unlam.paradigmas.tp.hogwarts.servicio;
 
-import unlam.paradigmas.tp.hogwarts.dto.Atraccion;
 import unlam.paradigmas.tp.hogwarts.dto.Producto;
-import unlam.paradigmas.tp.hogwarts.dto.Promocion;
 
 import java.util.Comparator;
 
 public class ProductoComparator implements Comparator<Producto> {
 
     @Override
-    public int compare(Producto prod1, Producto prod2) { // nueva version, reseñas?
-        boolean esPromocion1 = prod1.esPromocion();
-        boolean esPromocion2 = prod2.esPromocion();
+    public int compare(Producto producto1, Producto producto2) {
+        boolean producto1esPromocion = producto1.esPromocion(); //true
+        boolean producto2esPromocion = producto2.esPromocion(); //false
 
-        if (esPromocion1 && !esPromocion2)
+        if (producto1esPromocion && !producto2esPromocion)
             return -1;
-        else if (!esPromocion1 && esPromocion2)
+
+        if (!producto1esPromocion && producto2esPromocion)
             return 1;
 
         // Ambos son promociones o ambos son atracciones
-        if (esPromocion1 && esPromocion2) {
-            Promocion promocion1 = (Promocion) prod1;
-            Promocion promocion2 = (Promocion) prod2;
+        int cmpPrecio = Double.compare(producto2.getPrecio(), producto1.getPrecio());
+        if (cmpPrecio != 0)
+            return cmpPrecio;
 
-            int cmpPrecio = Double.compare(promocion2.getPrecio(), promocion1.getPrecio());
-            if (cmpPrecio != 0)
-                return cmpPrecio;
-
-            int cmpDuracion = Double.compare(promocion2.getDuracion(), promocion1.getDuracion());
-            return cmpDuracion;
-
-        } else {
-            Atraccion atraccion1 = (Atraccion) prod1;
-            Atraccion atraccion2 = (Atraccion) prod2;
-
-            int cmpPrecio = Double.compare(atraccion2.getPrecio(), atraccion1.getPrecio());
-            if (cmpPrecio != 0)
-                return cmpPrecio;
-
-            int cmpDuracion = Double.compare(atraccion2.getDuracion(), atraccion1.getDuracion());
-            return cmpDuracion;
-
-        }
-
+        return Double.compare(producto2.getDuracion(), producto1.getDuracion());
     }
-
 }
